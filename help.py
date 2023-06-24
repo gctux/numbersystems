f563e01a3fb0c4768de990b49037de92c4b58d02
Lutz Herrmann <lutz.herrmann@mailbox.org>
2023-06-24 12:13:06 +0200

    commit 001

--- help.py
+++ help.py
@@ -4,6 +4,25 @@ import tkinter as tk #python3
 root = tk.Tk() #create the tkinter window
 frame = HtmlFrame(root) #create HTML browser
 
-frame.load_website("about.html") #load a website
+html = '''
+<!DOCTYPE html>
+
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Meine erste Seite</title>
+  </head>
+  <body>
+    <p align = 'center'><h3 align = 'center'>Zahlsysteme umrechnen!</h3></p>
+    <p align = 'center'>&copy; Lutz Herrmann <br> Georgius-Agricola-Gymnasium Glauchau <br> 2022 bis 2023</p>
+    <p align = 'center'> Dieses Programm kommt OHNE JEDWEDE GARANTIE. </p>
+    <p align = 'center'> <a href='https://www.gnu.org/licenses/gpl-3.0.de.html'>GNU General Public License, Version 3 oder neuer</a> </p>
+    <p align = 'center'>Projektseite: </p>
+  </body>
+</html>
+'''
+
+frame.load_html(html) #load a file
+root.geometry('400x300')
 frame.pack(fill="both", expand=True) #attach the HtmlFrame widget to the parent window
 root.mainloop()
