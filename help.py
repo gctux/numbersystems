3c3809e502090fe3022a002f90d592ac04e8c548
Lutz Herrmann <lutz.herrmann@mailbox.org>
2023-06-27 06:42:00 +0200

    version 0.1

--- help.py
+++ help.py
@@ -1,38 +1,70 @@
 import tkinter as tk
-from tkinter import font as tkFont
+from tkhtmlview import HTMLScrolledText
 
-class RichText(tk.Text):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        default_font = tkFont.nametofont(self.cget("font"))
-
-        em = default_font.measure("m")
-        default_size = default_font.cget("size")
-        bold_font = tkFont.Font(**default_font.configure())
-        italic_font = tkFont.Font(**default_font.configure())
-        h1_font = tkFont.Font(**default_font.configure())
-
-        bold_font.configure(weight="bold")
-        italic_font.configure(slant="italic")
-        h1_font.configure(size=int(default_size*2), weight="bold")
-
-        self.tag_configure("bold", font=bold_font)
-        self.tag_configure("italic", font=italic_font)
-        self.tag_configure("h1", font=h1_font, spacing3=default_size)
-
-        lmargin2 = em + default_font.measure("\u2022 ")
-        self.tag_configure("bullet", lmargin1=em, lmargin2=lmargin2)
-
-    def insert_bullet(self, index, text):
-        self.insert(index, f"\u2022 {text}", "bullet")
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    text = RichText(root, width=40, height=15)
-    text.pack(fill="both", expand=True)
-
-    text.insert("end", "Rich Text Example\n", "h1")
-    text.insert("end", "Hello, world\n\n")
-    text.insert_bullet("end", "Item 1\n")
-    text.insert_bullet("end", "Item 2\n")
-    root.mainloop()
+root = tk.Tk()
+root.geometry("780x640")
+html_label = HTMLScrolledText(
+    root,
+    html="""
+      <h1 style="color: red; text-align: center"> Hello World </h1>
+      <h2> Smaller </h2>
+      <h3> Little Smaller </h3>
+      <h4> Little Little Smaller </h4>
+      <h5> Little Little Little Smaller </h5>
+      <h6> Little Little Little Little Smaller </h6>
+      <b>This is a Bold text</b><br/>
+      <strong>This is an Important text</strong><br/>
+      <i>This is a Italic text</i><br/>
+      <em>This is a Emphasized text</em><br/>
+      <em>This is a <strong>Strong Emphasized</strong>   text   </em>.<br/>
+      <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"></img>
+      <ul>
+      <li> One </li>
+        <ul>
+            <li> One.Zero </li>
+            <li> One.One </li>
+        </ul>
+      <li> Two </li>
+      <li> Three </li>
+      <li> Four </li>
+      </ul>
+      <h3> Paragraph </h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut quam sapien. Maecenas porta tempus mauris sed ullamcorper. Nulla facilisi. Nulla facilisi. Mauris tristique ipsum et efficitur lobortis. Sed pharetra ipsum non lacinia dignissim. Ut condimentum vulputate sem eget scelerisque. Curabitur ornare augue enim, sed volutpat enim finibus id. </p>
+      <h3>Table</h3
+      <table>
+          <tr>
+            <th style="background-color: silver">T Header 1</th>
+            <th style="background-color: silver">T Header 2</th>
+          </tr>
+          <tr>
+            <td>ABC</td>
+            <td><em>123</em></td>
+          </tr>
+          <tr>
+            <td>DEF</td>
+            <td><i>456</i></td>
+          </tr>
+      </table>
+      <h3> Preformatted text: spaces and newlines preserved </h3>
+      <pre style="font-family: Consolas; font-size: 80%">
+<b>Tags      Attributes          Notes</b>
+a         style, <span style="color: RoyalBlue">href         </span>-
+img       <u>src</u>, width, height  <i>experimental</i>
+ol        <span style="color: Tomato">style, type         </span>-
+ul        <i>style               </i>bullet glyphs only
+div       style               -
+      </pre>
+      <h3> Code: spaces and newlines ignored </h3>
+      <code style="font-family: Consolas; font-size: 80%">
+<b>Tags      Attributes          Notes</b><br/>
+a         style, <span style="color: RoyalBlue">href         </span>-
+img       <u>src</u>, width, height  <i>experimental</i>
+ol        <span style="color: Tomato">style, type         </span>-
+ul        <i>style               </i>bullet glyphs only
+div       style               -
+      </code>
+    """,
+)
+html_label.pack(fill="both", expand=True)
+html_label.fit_height()
+root.mainloop()
